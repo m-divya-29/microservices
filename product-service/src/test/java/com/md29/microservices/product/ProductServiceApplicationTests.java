948dfe5374dcf5eba02a88756c156c56eddfce86
Divya Mahankali <divya.gracie@gmail.com>
2025-03-27 10:40:13 +0530

    Product Service ends

--- product-service/src/test/java/com/md29/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/md29/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,51 @@
 package com.md29.microservices.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // Runs test on random port
 class ProductServiceApplicationTests {
+	@ServiceConnection // automatically sets mongodb connection uri from application.properties.
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+	@LocalServerPort // injects random port into this variable.
+	private Integer port;
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
 
+	static {
+		mongoDBContainer.start();
+	}
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				    "name": "Samsung S24 Ultra",
+				    "description": "Biggest camera",
+				    "price": "90000"
+				}
+				""";
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("Samsung S24 Ultra"))
+				.body("description", Matchers.equalTo("Biggest camera"))
+				.body("price", Matchers.equalTo(90000));
 	}
 
 }
