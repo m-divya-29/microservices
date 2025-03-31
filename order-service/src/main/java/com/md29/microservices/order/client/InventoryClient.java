6110310af592e27f6fd3e3dfa4175cb1cd2be06b
m-divya-29 <divya.gracie@gmail.com>
2025-04-01 01:00:35 +0530

    Parametrize inventory url, update tests

--- order-service/src/main/java/com/md29/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/md29/microservices/order/client/InventoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082")
+@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
     @GetMapping(value = "/api/inventory")
     public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
--- order-service/src/test/java/com/md29/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/md29/microservices/order/OrderServiceApplicationTests.java
@@ -1,16 +1,19 @@
 package com.md29.microservices.order;
 
+import com.md29.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0) //random port
 class OrderServiceApplicationTests {
 
 	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
@@ -31,12 +34,13 @@ class OrderServiceApplicationTests {
 	void shouldCreateOrder() {
 		String requestBody = """
 				{
-				     "orderNumber": "iPhone16",
-				     "skuCode": "Made for Apple Intelligence",
+				     "orderNumber": "1234",
+				     "skuCode": "iphone_15",
 				     "price": "999",
-				     "quantity": 1
+				     "quantity": 12
 				 }
 				""";
+		InventoryClientStub.stubInventoryCall("iphone_15", 12);
 		RestAssured.given()
 				.contentType("application/json")
 				.body(requestBody)
