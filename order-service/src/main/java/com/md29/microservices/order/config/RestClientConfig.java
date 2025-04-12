1235a73b91d6613e73fd18eca13266d30e8c65f1
m-divya-29 <divya.gracie@gmail.com>
2025-04-12 17:27:19 +0530

    Circuit breaker: timeout configuration order service

--- order-service/src/main/java/com/md29/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/md29/microservices/order/config/RestClientConfig.java
@@ -1,12 +1,21 @@
 package com.md29.microservices.order.config;
 
 import com.md29.microservices.order.client.InventoryClient;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -17,10 +26,39 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+//    private ClientHttpRequestFactory getClientRequestFactory() {
+//        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+//        factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+//        factory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
+//        return factory;
+//    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(3))
+                .setResponseTimeout(Timeout.ofSeconds(3))
+                .build();
+
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(new PoolingHttpClientConnectionManager())
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        factory.setHttpClient(httpClient);
+        return factory;
+
+
     }
 
 }
